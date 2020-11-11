d20b7df1936fe0acc52e464d3908779b3f48bddb
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2020-11-11 20:04:02 +0530

    bootstrap css added

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 
 
-def test(request):
-	return HttpResponse("Sever started on localhost")
+def testServer(request):
+	return HttpResponse("Sever has started")
 
 def index(request):
 	return render(request,'index.html')
