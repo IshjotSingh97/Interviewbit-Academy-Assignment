01938f17a905880df0d32bd40866af32ad397f17
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2020-11-12 02:53:15 +0530

    card added for interview schedule

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 from datetime import datetime
 from .models import *
+from collections import defaultdict
 
 def testserver(request):
 	return HttpResponse("Sever has started successfully")
@@ -10,28 +11,54 @@ def index(request):
 	return render(request,'index.html')
 
 def onsubmit(request):
-	# logic for participants
+
 	participants = request.POST['useremails'].split(',')
-	for i in range(len(participants)):
-		participant = Participant()
-		participant.useremail = participants[i]
-		participant.save()
+
+	if isValidCount(list(participants)) == False:
+		data = {
+		"errormsg" : "Number of participants are less than 2"
+		}
+		return render(request,'index.html',context=data)
+	
 	# logic for interview
 	interview = Interview()
 	interview.title = request.POST['title']
 	interview.date = request.POST['date']
 	interview.starttime = request.POST['starttime']
-	interview.endttime = request.POST['endtime']
+	interview.endtime = request.POST['endtime']
 	interview.save()
-	return render(request,'index.html')
+
+	# logic for participants
+	for i in range(len(participants)):
+		participant = Participant()
+		participant.useremail = participants[i]
+		participant.save()
+
+		# logic for schedule
+		schedule = Schedule()
+		schedule.participant_id = participant.id
+		schedule.interview_id = interview.id
+		schedule.save()
+
+	d = collections.defaultdict(lambda : [])
+
+	for schedule in Schedule.objects.all():
+		key = Interview.objects.get(id=schedule.interview_id)
+		value = Participant.objects.get(id=schedule.participant_id)
+		d[key].append(value)
+
+	data = {
+		"d" : d
+	}
 	
+	return render(request,'index.html',context=data)
+	
+
 
 def isValidCount(participants):
 	if len(participants) < 2:
-		errormsg = "Number of participants are less than 2"
 		return False
 	else:
-		validmsg = "This schedule is valid"
 		return True
 
 # Create your views here.
