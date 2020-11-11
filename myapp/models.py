c8abeafc4503c947a88951031087a776ca5bddd2
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2020-11-11 21:14:30 +0530

    database schema defined models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Participant(models.Model):
+	useremail = models.CharField(max_length=30)
+
+class Interview(models.Model):
+	title = models.CharField(max_length=30)
+	starttime = DateTimeField()
+	endtime = DateTimeField()
+
+class Schedule(models.Model):
+	participant = models.ForeignKey(Participant,on_delete=models.CASCADE)
+	interview = models.ForeignKey(Interview,on_delete=models.CASCADE)
