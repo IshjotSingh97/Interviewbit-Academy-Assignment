ad6cefe369f7ffa44c2d40bd1ca54a816faa5516
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2020-11-11 20:39:08 +0530

    added check condition 2 for error handling

--- myapp/views.py
+++ myapp/views.py
@@ -3,12 +3,21 @@ from django.http import JsonResponse,HttpResponse
 
 
 def testserver(request):
-	return HttpResponse("Sever has started")
+	return HttpResponse("Sever has started successfully")
 
 def index(request):
 	return render(request,'index.html')
 
 def onsubmit(request):
 	return JsonResponse(request.POST)
+
+def isValidCount(participants):
+	participants = participants.split(',')
+	if len(participants) < 2:
+		errormsg = "Number of participants are less than 2"
+		return (False,errormsg)
+	else:
+		validmsg = "This schedule is valid"
+		return (True,validmsg)
 
 # Create your views here.
